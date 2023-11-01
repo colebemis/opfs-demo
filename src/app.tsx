40ca9ce479692591fbd4b5c47c9d5a844928e047
Cole Bemis <colebemis@github.com>
2023-11-01 00:35:50 -0700

    Remove useEffect

--- src/app.tsx
+++ src/app.tsx
@@ -2,21 +2,25 @@ import git from "isomorphic-git";
 import http from "isomorphic-git/http/web";
 import { FsaNodeFs } from "memfs/lib/fsa-to-node";
 import { IFileSystemDirectoryHandle } from "memfs/lib/fsa/types";
-import React from "react";
 
 // Reference: https://github.com/streamich/memfs/blob/c8bfa38aa15f1d3c9f326e9c25c8972326193a26/demo/git-opfs/main.ts
 const rootDir =
   navigator.storage.getDirectory() as unknown as Promise<IFileSystemDirectoryHandle>;
+
 const fs = new FsaNodeFs(rootDir);
 
-async function init() {
+init(fs);
+
+async function init(fs: FsaNodeFs) {
   try {
+    // This will throw if the directory doesn't exist
     const files = await fs.promises.readdir("/tmp");
+
     console.log("Already cloned");
     console.log(files);
   } catch (error) {
-    console.error(error);
     console.log("Cloning...");
+
     await git.clone({
       fs,
       http,
@@ -29,7 +33,9 @@ async function init() {
       // https://isomorphic-git.org/docs/en/onAuth
       // onAuth: () => ({ username: <token> }),
     });
+
     const files = await fs.promises.readdir("/tmp");
+
     console.log(files);
   }
 }
@@ -37,8 +43,5 @@ async function init() {
 // TODO: Display list of files in OPFS
 
 export function App() {
-  React.useEffect(() => {
-    init();
-  }, []);
   return <div>Hello world</div>;
 }
