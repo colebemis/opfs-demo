a8693053fb4aaf29b73d8dbb1ef1448f6b5ba860
Cole Bemis <colebemis@github.com>
2023-10-31 23:28:06 -0700

    Clean up

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,22 @@
 import git from "isomorphic-git";
-import FS from "@isomorphic-git/lightning-fs";
 import http from "isomorphic-git/http/web";
+import { FsaNodeFs } from "memfs/lib/fsa-to-node";
+import { IFileSystemDirectoryHandle } from "memfs/lib/fsa/types";
 import React from "react";
-// import { Buffer } from "buffer";
 
-// self.Buffer = Buffer;
-
-const fs = new FS("fs").promises;
+// Reference: https://github.com/streamich/memfs/blob/c8bfa38aa15f1d3c9f326e9c25c8972326193a26/demo/git-opfs/main.ts
+const rootDir =
+  navigator.storage.getDirectory() as unknown as Promise<IFileSystemDirectoryHandle>;
+const fs = new FsaNodeFs(rootDir);
 
 async function init() {
   try {
-    const files = await fs.readdir("/tmp");
-    console.log("already cloned");
+    const files = await fs.promises.readdir("/tmp");
+    console.log("Already cloned");
     console.log(files);
   } catch (error) {
     console.error(error);
+    console.log("Cloning...");
     await git.clone({
       fs,
       http,
@@ -24,11 +26,15 @@ async function init() {
       ref: "main",
       singleBranch: true,
       depth: 10,
+      // https://isomorphic-git.org/docs/en/onAuth
+      // onAuth: () => ({ username: <token> }),
     });
-    const files = await fs.readdir("/tmp");
+    const files = await fs.promises.readdir("/tmp");
     console.log(files);
   }
 }
+
+// TODO: Display list of files in OPFS
 
 export function App() {
   React.useEffect(() => {
