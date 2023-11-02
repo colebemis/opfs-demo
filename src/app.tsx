df6bec66921227419d36be206913da73b3666dc3
Cole Bemis <colebemis@github.com>
2023-11-01 21:11:22 -0700

    List files in the UI

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,9 @@ import git from "isomorphic-git";
 import http from "isomorphic-git/http/web";
 import { FsaNodeFs } from "memfs/lib/fsa-to-node";
 import { IFileSystemDirectoryHandle } from "memfs/lib/fsa/types";
+import React from "react";
+
+const REPO_DIR = "/repo";
 
 // Reference: https://github.com/streamich/memfs/blob/c8bfa38aa15f1d3c9f326e9c25c8972326193a26/demo/git-opfs/main.ts
 const rootDir =
@@ -14,17 +17,13 @@ init(fs);
 async function init(fs: FsaNodeFs) {
   try {
     // This will throw if the directory doesn't exist
-    const files = await fs.promises.readdir("/tmp");
-
     console.log("Already cloned");
-    console.log(files);
   } catch (error) {
     console.log("Cloning...");
-
     await git.clone({
       fs,
       http,
-      dir: "/tmp",
+      dir: REPO_DIR,
       corsProxy: "https://cors.isomorphic-git.org",
       url: "https://github.com/lumen-notes/notes-template",
       ref: "main",
@@ -33,15 +32,68 @@ async function init(fs: FsaNodeFs) {
       // https://isomorphic-git.org/docs/en/onAuth
       // onAuth: () => ({ username: <token> }),
     });
-
-    const files = await fs.promises.readdir("/tmp");
-
-    console.log(files);
   }
 }
 
-// TODO: Display list of files in OPFS
+/** Recursively list contents of a directory */
+async function* walk(
+  dir: string,
+  pattern: RegExp = /.*/
+): AsyncGenerator<string> {
+  const files = await fs.promises.readdir(dir);
+
+  for (const file of files) {
+    // Ignore .git directory
+    if (file === ".git") continue;
+
+    const path = `${dir}/${file}`;
+    const stat = await fs.promises.stat(path);
+
+    if (stat.isDirectory()) {
+      yield* walk(path, pattern);
+    } else if (stat.isFile() && pattern.test(file)) {
+      yield path;
+    }
+  }
+}
+
+/** Convert a generator to an array */
+async function toArray<T>(gen: AsyncGenerator<T>) {
+  const arr: T[] = [];
+  for await (const item of gen) {
+    arr.push(item);
+  }
+  return arr;
+}
 
 export function App() {
-  return <div>Hello world</div>;
+  const [files, setFiles] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    let ignore = false;
+
+    (async () => {
+      try {
+        const files = await toArray(walk(REPO_DIR, /\.md$/));
+
+        if (!ignore) {
+          setFiles(files);
+        }
+      } catch (error) {
+        if (!ignore) {
+          console.error(error);
+        }
+      }
+    })();
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  return (
+    <div>
+      <pre>{JSON.stringify(files, null, 2)}</pre>
+    </div>
+  );
 }
