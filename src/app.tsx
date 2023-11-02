5db73c3bfb7ba3fa30f4521257f69b8ea1466d2e
Cole Bemis <colebemis@github.com>
2023-11-02 00:27:37 -0700

    Update state machine

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,55 @@
 import { useMachine } from "@xstate/react";
 import git from "isomorphic-git";
 import http from "isomorphic-git/http/web";
-import { createMachine, fromPromise, assign } from "xstate";
+import LagRadar from "react-lag-radar";
+import { assign, createMachine, fromPromise } from "xstate";
 import { fs } from "./fs";
 
+// TODO: Configure synchronous fs adatper
 // TODO: Run file system and git commands in a web worker to avoid blocking the UI thread
 
 const ROOT_DIR = "/tmp";
+const DEFAULT_BRANCH = "main";
 
 type Context = {
+  files: string[];
   error: Error | null;
 };
 
 type Event = { type: "SELECT_REPO"; repo: string };
 
 const gitMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5RQJYBcB0YC2AHNAngMQDCAMgPIByAogNoAMAuoqLgPazorsB2rIAB6IAtAEYAzBjEMArABYAbAA4A7LIA0IAogBM82RlWqJ8ieoC+FrakwBjADZ8UvKEQh8wGFwDd2Aay9HT0YWJBAOLjQefnDhBF1FeQxZBlVlMV1NbUQATl0rG3QMYN4XNzAAJ0r2SoxcBwBDNAAzWuwSp14wUIFI7j4BeMTk1PTM7J0Ehl0Uq2sQXnYIOAFbPs4B2NB4kUUxaTklNUnRRUKQWyw8Qg2omKHEZRStKbFZVSMxE4ur0vK7ltHggGK89LlRr9iigIA4wIDooM4oh0hgJLllGZ1GCEGIMXMFlcAK68fxLADuvBo1VqCIeyIQ8l0OIyBKsQA */
+  /** @xstate-layout N4IgpgJg5mDOIC5RQJYBcB0KB26UEMAbFALxygGIIB7bMLbAN2oGt6d0BtABgF1FQAB2qw8tASAAeiAEzcAbBgUyArAEYZAFgDM8lds0KANCACeiTfM0Y12mQA4ZVlfe4B2ewE43AXx8nUTA40AmIybEowACco6iiMQUJ8NAAzOIBbBi4+CWFREPEkKVkFJXlVDR09A2MzRFs1Gyd9XRV5Tzs-APQMMHTBNFMKAGUAUQAZUYBhABUAfQAlUYAFAHkefiK8sWwJaQQda1UPNw61T1VtNRNzBA1PDHK9TTU3eW5tbheZLpBAjAAxoRaOQqLR2ExWPQgeCNrkRDs9ohTtwbG1dKd5Lp7Jp7DdEHZGp5lNoXNxuN53PJfv8YbgIhRorF4olkmkopk6WA4VsEQVdkV9ii0Vj5Jjsbj8Xc3G4MJ59JVdNxbCoVDSeigIIQwCMJtN5ks1jyhHyUIVQELiSKMZ5RTi8XUEDI3I1DOUZA4FJpNJ57OrMABXbAsbDUADu2FGMTiusms0WK3WOV5+TNAotyKtanRYttEs8UvsjRU5NL8ns8nkah0fn8IFDEDgEkC8NT5uKCAAtPIpZ2VHLPIOh8Oh9p-VkQkRSORW4jBRYZFKNKj7G99FZ7C5PvY-XX-n0BrcTW30x3tAYbKvVVWZOdiYvHbZURpnq93p9vuO6TOU3OMwh3kaCltHsAwi2VNx9CXUonhUNw5HPTxszUNRx01bVZ35JEEHlRRBw9QwXDFbgVClAxFBA518J0HExz3HogxDcNI2jKJMLTbDcIHAiSIrNwSKXWwlBUQcSPPPQ1EcXc-CAA */
   id: "git",
   types: {} as { context: Context; events: Event },
   context: {
+    files: [],
     error: null,
   },
-  initial: "empty",
+  initial: "initializing",
   states: {
+    initializing: {
+      invoke: {
+        id: "init",
+        src: fromPromise(async () => {
+          const files = await git.listFiles({
+            fs,
+            dir: ROOT_DIR,
+            ref: DEFAULT_BRANCH,
+          });
+
+          return { files };
+        }),
+        onDone: {
+          target: "idle",
+          actions: assign({
+            files: ({ event }) => event.output.files as string[],
+          }),
+        },
+        // If git.listFiles() fails, the repo is empty
+        onError: "empty",
+      },
+    },
     empty: {
       on: {
         SELECT_REPO: "cloning",
@@ -35,11 +62,11 @@ const gitMachine = createMachine({
           return { repo: event.repo };
         },
         src: fromPromise(async ({ input }) => {
-          // Clean up previous clone
+          // Remove existing repo
           console.log(`$ rm -rf ${ROOT_DIR}`);
           await fs.promises.rm(ROOT_DIR, { recursive: true, force: true });
 
-          // Clone the repo
+          // Clone repo
           console.log(
             `$ git clone https://github.com/${input.repo}.git ${ROOT_DIR}`
           );
@@ -49,15 +76,30 @@ const gitMachine = createMachine({
             dir: ROOT_DIR,
             corsProxy: "https://cors.isomorphic-git.org",
             url: `https://github.com/${input.repo}`,
-            ref: "main",
+            ref: DEFAULT_BRANCH,
             singleBranch: true,
             depth: 1,
             onMessage: console.log,
             // https://isomorphic-git.org/docs/en/onAuth
             // onAuth: () => ({}),
           });
+
+          // List files
+          // TODO: Investigate git.walk() as a more performant alternative to git.listFiles()
+          const files = await git.listFiles({
+            fs,
+            dir: ROOT_DIR,
+            ref: DEFAULT_BRANCH,
+          });
+
+          return files;
         }),
-        onDone: "idle",
+        onDone: {
+          target: "idle",
+          actions: assign({
+            files: ({ event }) => event.output as string[],
+          }),
+        },
         onError: {
           target: "unknownError",
           actions: assign({
@@ -87,7 +129,8 @@ export function App() {
 
   return (
     <div>
-      <pre>{JSON.stringify(state.value)}</pre>
+      <LagRadar />
+      <pre>State: {state.value.toString()}</pre>
       {state.context.error ? (
         <pre>Error: {state.context.error.message}</pre>
       ) : null}
@@ -96,7 +139,7 @@ export function App() {
           event.preventDefault();
           const formData = new FormData(event.currentTarget);
           const repo = formData.get("repo");
-          send({ type: "SELECT_REPO", repo: `${repo}` });
+          send({ type: "SELECT_REPO", repo: String(repo) });
         }}
       >
         <input
@@ -108,6 +151,7 @@ export function App() {
         />
         <button type="submit">Select</button>
       </form>
+      <pre>Files: {JSON.stringify(state.context.files, null, 2)}</pre>
     </div>
   );
 }
